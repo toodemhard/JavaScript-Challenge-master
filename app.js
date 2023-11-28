388d00b70e93b9d80625145834c271799d2c3331
toodemhard <lailokkay@gmail.com>
2023-11-28 15:43:19 +0000

    Restructure concurrency
    Retry fetches run concurrently with other fetches.

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+    const stockSymbols = await stocks.getStocks()
+    res.send({ stockSymbols })
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
+    const { params: { symbol } } = req
 
-  let data;
-  try{
-    data = await stocks.getStockPoints(symbol, new Date())
-  } catch(err) {
-    res.status(500);
-    data = "failed to retrieve stock"
-  }
-  res.send(data)
+    let data;
+    try {
+        data = await stocks.getStockPoints(symbol, new Date())
+    } catch (err) {
+        res.status(500);
+        data = "failed to retrieve stock"
+    }
+    res.send(data)
 })
 
 app.listen(3000, () => console.log('Server is running!'))
--- static/scripts.js
+++ static/scripts.js
@@ -1,96 +1,89 @@
 const canvas = document.getElementById('chart')
 const ctx = canvas.getContext('2d')
 
-function drawLine (start, end, style) {
-  ctx.beginPath()
-  ctx.strokeStyle = style || 'black'
-  ctx.moveTo(...start)
-  ctx.lineTo(...end)
-  ctx.stroke()
+function drawLine(start, end, style) {
+    ctx.beginPath()
+    ctx.strokeStyle = style || 'black'
+    ctx.moveTo(...start)
+    ctx.lineTo(...end)
+    ctx.stroke()
 }
 
-function drawTriangle (apex1, apex2, apex3) {
-  ctx.beginPath()
-  ctx.moveTo(...apex1)
-  ctx.lineTo(...apex2)
-  ctx.lineTo(...apex3)
-  ctx.fill()
+function drawTriangle(apex1, apex2, apex3) {
+    ctx.beginPath()
+    ctx.moveTo(...apex1)
+    ctx.lineTo(...apex2)
+    ctx.lineTo(...apex3)
+    ctx.fill()
 }
 
 const spinner = document.querySelector('.rotate');
 
-async function fetchStocks() {
-  const stockList = await (await fetch("/stocks")).json();
+async function fetchRetry(name) {
+    const url = "/stocks/" + name;
+    let response = await fetch(url);
 
-  let stockNames = [];
-  let stockReqs = [];
-  stockList["stockSymbols"].forEach(async (stock) => {
-    try {
-      stockNames.push(stock);
-      stockReqs.push(fetch("/stocks/"+stock));
-    } catch(err) {
-      console.log("fetch failed");
-    }
-  })
-
-  let stockResponses = await Promise.all(stockReqs);
-  for (let i=0; i<stockResponses.length; i++) {
     let retries = 0;
-    while (stockResponses[i].status != 200 && retries < 3) {
-      retries++;
-
-      console.log(`failed: retying ${retries} times...`);
-      stockResponses[i] = await fetch("stocks/"+stockNames[i]);
+    while (response.status != 200 && retries < 3) {
+        retries++;
+        response = await fetch(url);
+        console.log(`${name} failed: retying ${retries} times...`);
     }
-    
-    if (stockResponses[i].status != 200) {
-      console.log("timeout: skipping this stock...");
-      continue;
+
+    return response;
+}
+
+async function fetchStocks() {
+    const stockNames = (await (await fetch("/stocks")).json())["stockSymbols"];
+    const stockReqs = [];
+
+    stockNames.forEach((stock) => {
+        stockReqs.push(fetchRetry(stock));
+    })
+
+    const stockResponses = await Promise.all(stockReqs);
+
+    spinner.classList.remove("rotate");
+
+    for (let i = 0; i < stockResponses.length; i++) {
+        console.log(`${stockNames[i]}: `);
+        const response = stockResponses[i];
+        if (response.status == 200) {
+            const stock = await response.json();
+            drawStock(stock);
+            stock.forEach((point) => {
+                console.log(`value: ${point["value"]}, time: ${point["timestamp"]}`);
+            });
+        } else {
+            console.log(`failed to fetch`);
+        }
     }
-  }
-
-  spinner.classList.remove("rotate");
-
-  for (let i=0; i<stockResponses.length; i++) {
-    console.log(`${stockNames[i]}: `);
-
-    const res = stockResponses[i];
-    try {
-      let stock = await res.json();
-      drawStock(stock);
-      stock.forEach((point) => {
-        console.log(`value: ${point["value"]}, time: ${point["timestamp"]}`);
-      });
-    } catch(err) {
-      console.log("failed to fetch");
-    }
-  }
 }
 
 function drawStock(points) {
-  if (points.length <= 0) {
-    return;
-  }
+    if (points.length <= 0) {
+        return;
+    }
 
-  const minX = 50;
-  const maxX = 950;
-  const rangeX = maxX - minX;
-  const maxY = 550;
-  const scaleY = 4;
-  startTime = points[0]["timestamp"];
-  timeRange = points[points.length-1]["timestamp"] - startTime;
-  for (let i=0; i<points.length-1; i++){
-    const p1 = points[i];
-    const p2 = points[i+1];
-    t1 = (p1["timestamp"] - startTime) / timeRange * rangeX + minX;
-    t2 = (p2["timestamp"] - startTime) / timeRange * rangeX + minX;
-    
-    drawLine([t1, maxY - p1["value"] * scaleY] , [t2, maxY - p2["value"] * scaleY]);
-  }
+    const minX = 50;
+    const maxX = 950;
+    const rangeX = maxX - minX;
+    const maxY = 550;
+    const scaleY = 4;
+    startTime = points[0]["timestamp"];
+    timeRange = points[points.length - 1]["timestamp"] - startTime;
+    for (let i = 0; i < points.length - 1; i++) {
+        const p1 = points[i];
+        const p2 = points[i + 1];
+        t1 = (p1["timestamp"] - startTime) / timeRange * rangeX + minX;
+        t2 = (p2["timestamp"] - startTime) / timeRange * rangeX + minX;
+
+        drawLine([t1, maxY - p1["value"] * scaleY], [t2, maxY - p2["value"] * scaleY]);
+    }
 }
 
 async function main() {
-  fetchStocks();
+    fetchStocks();
 }
 
 main();
